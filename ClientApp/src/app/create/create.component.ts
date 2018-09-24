30ae9219f4a86744f3404a66b2c2a860620f6570
Max Vandenesse <mvandenesse@amctechnology.com>
2018-09-24 14:38:28 -0400

    remove any as type

--- ClientApp/src/app/create/create.component.ts
+++ ClientApp/src/app/create/create.component.ts
@@ -6,7 +6,7 @@ import { LoggerService } from './../logger.service';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-  @Output() CreateNewEntity: EventEmitter<any> = new EventEmitter<any>();
+  @Output() CreateNewEntity: EventEmitter<string> = new EventEmitter<string>();
   maximizeCreate: boolean;
   constructor(private loggerService: LoggerService) {
     this.maximizeCreate = true;
