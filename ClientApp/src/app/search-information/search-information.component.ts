d7a41687de1f124c793c20702114740f129c35bd
Max Vandenesse <mvandenesse@amctechnology.com>
2018-11-09 16:23:26 -0500

    replace member variables with service: search info

--- ClientApp/src/app/search-information/search-information.component.ts
+++ ClientApp/src/app/search-information/search-information.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import * as api from '@amc/application-api';
 import { LoggerService } from './../logger.service';
+import { StorageService } from '../storage.service';
 @Component({
   selector: 'app-search-information',
   templateUrl: './search-information.component.html',
   styleUrls: ['./search-information.component.css']
 })
 export class SearchInformationComponent {
-  @Input() searchReturnedSingleResult: boolean;
-  @Input() searchRecordList: Array<api.IRecordItem>;
   @Output() agentSelectedCallerInformation: EventEmitter<string> = new EventEmitter();
   isSearchInformationMaximized: boolean;
   imageLocation: string;
-  constructor(private loggerService: LoggerService) {
+  constructor(private loggerService: LoggerService, protected storageService: StorageService) {
     this.loggerService.logger.logDebug('searchInformationComponent: Constructor start');
     this.isSearchInformationMaximized = true;
     this.loggerService.logger.logDebug('searchInformationComponent: Constructor complete');
@@ -43,9 +42,9 @@ export class SearchInformationComponent {
     return name;
   }
   protected getRecord(id) {
-    for (let i = 0; i < this.searchRecordList.length; i++) {
-      if (this.searchRecordList[i].id === id) {
-        return this.searchRecordList[i];
+    for (let i = 0; i < this.storageService.searchRecordList.length; i++) {
+      if (this.storageService.searchRecordList[i].id === id) {
+        return this.storageService.searchRecordList[i];
       }
     }
   }
