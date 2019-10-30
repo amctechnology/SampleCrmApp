91b121784f8c553fad81cf25c0be19d50fd8a95b
Matthew Pointer <pointermc@mymail.vcu.edu>
2019-10-30 17:29:23 -0400

    Working on New multi match UI

--- ClientApp/src/app/search-information/search-information-salesforce.component.ts
+++ ClientApp/src/app/search-information/search-information-salesforce.component.ts
@@ -15,17 +15,23 @@ export class SearchInformationSalesforceComponent implements OnInit {
   imageLocation: string;
   singleMatchIconSrc: string;
   dataToShow: any;
-
+  multiMatchDataToShow: any[];
   constructor(private loggerService: LoggerService, protected storageService: StorageService) {
     this.loggerService.logger.logDebug('searchInformationComponent: Constructor start');
     this.isSearchInformationMaximized = true;
     this.loggerService.logger.logDebug('searchInformationComponent: Constructor complete');
     this.dataToShow = null;
+    this.multiMatchDataToShow = [];
   }
   ngOnInit() {
     if (this.searchRecordList.length === 1) {
       this.dataToShow = this.parseSearchRecordForNameSingleMatch(this.searchRecordList[0]);
+    } else if (this.searchRecordList.length > 1) {
+      for (let i = 0; i < this.searchRecordList.length; i++) {
+        this.multiMatchDataToShow.push(this.parseSearchRecordForNameMultiMatch(this.searchRecordList[i]));
+      }
     }
+    console.log('asdf');
   }
 
   protected expandCallerInformationSection() {
@@ -102,6 +108,50 @@ export class SearchInformationSalesforceComponent implements OnInit {
               ([sLayoutInfo.DisplayFields[j].DevName, displayRecord]));
             }
             results.push(displayRecord);
+          }
+        }
+      }
+    }
+    return results;
+    }
+    return '';
+  }
+
+  protected parseSearchRecordForNameMultiMatch(searchRecord: api.IRecordItem) {
+    const results = [];
+    let src = '';
+    if (searchRecord.type) {
+      if (searchRecord.type.toUpperCase() === 'CONTACT') {
+        src = '../../assets/images/Icon_Contact.png';
+      } else if (searchRecord.type.toUpperCase() === 'ACCOUNT') {
+        src = '../../assets/images/Icon_Account.png';
+      } else if (searchRecord.type.toUpperCase() === 'LEAD') {
+        src = '../../assets/images/Icon_Lead.png';
+      } else {
+        src = '../../assets/images/Miscellaneous_Icon.png';
+      }
+    }
+    if (this.searchLayout && this.searchLayout.layouts) {
+      const sLayoutInfo = this.searchLayout.layouts[0][this.storageService.getActivity().CallType].
+      find(i => i.DevName === searchRecord.type);
+      for (let j = 0; j < sLayoutInfo.DisplayFields.length; j++) {
+      if (sLayoutInfo.DisplayFields && sLayoutInfo.DisplayFields[j].DevName) {
+        const nameKey = sLayoutInfo.DisplayFields[j].DevName;
+        const keys = Object.keys(searchRecord.fields);
+        for (let i = 0; i < keys.length; i++) {
+          if (searchRecord.fields[keys[i]] && searchRecord.fields[keys[i]].DevName === nameKey) {
+            let displayRecord = searchRecord.fields[keys[i]].Value;
+            if (j === 0) {
+              displayRecord = (searchRecord.displayName ? ([searchRecord.displayName, displayRecord]) :
+              ([searchRecord.type, displayRecord]));
+            } else {
+              displayRecord = (sLayoutInfo.DisplayFields[j].DisplayName ?
+              ([sLayoutInfo.DisplayFields[j].DisplayName, displayRecord]) :
+              ([sLayoutInfo.DisplayFields[j].DevName, displayRecord]));
+            }
+            displayRecord.push(src);
+            results.push(displayRecord);
+            return results;
           }
         }
       }
